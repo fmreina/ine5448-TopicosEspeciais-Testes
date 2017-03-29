52f0e8f9da78ce21ad16e2c45462f3781d4d57f2
fmreina <fmreina@hotmail.com>
2017-03-28 23:14:37 -0300

    aula 4

--- codigo/SistemaBancario.HamcrestTests/src/hamcresttests/HamcrestTest.java
+++ codigo/SistemaBancario.HamcrestTests/src/hamcresttests/HamcrestTest.java
@@ -1,39 +1,51 @@
 package hamcresttests;
 
-import static org.junit.Assert.*;
-
-import javax.naming.InvalidNameException;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
-import org.omg.CORBA.DynAnyPackage.InvalidValue;
 
 import br.ufsc.ine.leb.sistemaBancario.Agencia;
 import br.ufsc.ine.leb.sistemaBancario.Banco;
 import br.ufsc.ine.leb.sistemaBancario.Conta;
-import br.ufsc.ine.leb.sistemaBancario.Dinheiro;
-import br.ufsc.ine.leb.sistemaBancario.Entrada;
 import br.ufsc.ine.leb.sistemaBancario.Moeda;
-import br.ufsc.ine.leb.sistemaBancario.Saida;
 import br.ufsc.ine.leb.sistemaBancario.SistemaBancario;
-import br.ufsc.ine.leb.sistemaBancario.Transacao;
-import br.ufsc.ine.leb.sistemaBancario.TransacaoNaoRealizada;
-import br.ufsc.ine.leb.sistemaBancario.ValorMonetario;
 
 public class HamcrestTest {
 
 	@Test
-	public void comparaConta() throws Exception {
+	public void comparaContaIgual() throws Exception {
+		// configuração
 		SistemaBancario sis = new SistemaBancario();
 		Banco bb = sis.criarBanco("Banco do Brasil", Moeda.BRL);
 		Agencia bbCentro = bb.criarAgencia("Centro");
 
+		// exercício
 		Conta contaMaria = bbCentro.criarConta("Maria");
-		ValorMonetario zero = new ValorMonetario(Moeda.BRL);
-
-		assertEquals("0001-5", contaMaria.obterIdentificador());
-		assertEquals("Maria", contaMaria.obterTitular());
-		assertEquals(zero, contaMaria.calcularSaldo());
-		assertEquals(bbCentro, contaMaria.obterAgencia());
+		Conta contaJoao = bbCentro.criarConta("João");
+		
+		// verificação
+		assertThat(contaMaria, new ContaMatcher(contaMaria.obterIdentificador(), contaMaria.obterTitular(),
+				contaMaria.obterAgencia().obterBanco().obterNome(), contaMaria.obterAgencia().obterNome()));
+		
+		// finalização
 	}
-	
+
+	@Test
+	public void comparaContaDiferente() throws Exception {
+		// configuração
+		SistemaBancario sis = new SistemaBancario();
+		Banco bb = sis.criarBanco("Banco do Brasil", Moeda.BRL);
+		Agencia bbCentro = bb.criarAgencia("Centro");
+
+		// exercício
+		Conta contaMaria = bbCentro.criarConta("Maria");
+		Conta contaJoao = bbCentro.criarConta("João");
+		
+		// verificação
+		assertThat(contaMaria, not(new ContaMatcher(contaJoao.obterIdentificador(), contaJoao.obterTitular(),
+				contaJoao.obterAgencia().obterBanco().obterNome(), contaJoao.obterAgencia().obterNome())));
+		
+		// finalização
+	}
 }
